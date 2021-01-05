e79799b94a38c32f275714e9e61b5b9615f08043
MahyaR-Kd <mahyarkakvand96@gmail.com>
2021-01-04 20:30:40 -0500

    connect app to database

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, Response, session, abort
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
-import config
-
-
+from passlib.hash import sha256_crypt
+import psycopg2
+from models import User
+from config import *
 
 app = Flask(__name__)
 
 # config
-app.config.update(SECRET_KEY = 'secret_xxx' )
+app.config.update(SECRET_KEY = SECRET_KEY)
 
- # flask-login
+#database
+
+
+# table_name = 'users'
+# cur.execute("DROP TABLE IF EXISTS users;")
+# cur.execute("CREATE TABLE users(%s, %s, %s, %s, %s);" % ("ID INT PRIMARY KEY","FIRSTNAME TEXT", "LASTNAME TEXT", "EMAIL  TEXT", "PASSWORD TEXT"))
+
+# conn.commit()
+
+
+# flask-login
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
-# silly user model
-class User(UserMixin):
-
-    def __init__(self, id):
-        self.id = id
-        
-    def __repr__(self):
-        return "%d" % (self.id)
-
-
 # create some users with ids 1 to 20       
-user = User(0)
+
+
+# main site route
 
 @app.route('/')
 def main_page():
@@ -48,6 +51,8 @@ def contact_page():
     return render_template('site/contact.html')
 
 
+# dashboard route
+
 
 @app.route('/dash')
 @login_required
@@ -58,26 +63,81 @@ def dash_page():
 
 
 @app.route("/login", methods=["GET", "POST"])
-def login():
+def login():  # sourcery skip: remove-unnecessary-else, swap-if-else-branches
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']        
-        if username == config.USERNAME and password == config.PASSWORD:
+        db = psycopg2.connect(database = PSQL_DB_NAME, user = PSQL_USERNAME, password = PSQL_PASSWORD, host = PSQL_HOST, port = PSQL_PORT)
+        cur = db.cursor() 
+        
+        emailaddress = request.form['emailaddress']
+        password = request.form['password'] 
+
+        cur.execute("select * from users where to_tsvector(email) @@ to_tsquery(%s);", (emailaddress,))
+        email_exist = bool(cur.rowcount)
+        
+        cur.execute("select password from users where to_tsvector(email) @@ to_tsquery(%s);", (emailaddress,))
+        usr_pass = cur.fetchall()
+        usr_pass = usr_pass[0][0]
+        usr_pass = usr_pass.replace(' ','')
+
+        cur.execute("select register from users where to_tsvector(email) @@ to_tsquery(%s);", (emailaddress,))
+        usr_register = cur.fetchall()
+        usr_register = usr_register[0][0]
+        
+        cur.execute("select id from users where to_tsvector(email) @@ to_tsquery(%s);", (emailaddress,))
+        usr_id = cur.fetchall()
+        usr_id = usr_id[0][0]
+
+        # usr_id = cur.fetchall()[0]
+
+        user = User(usr_id)
+
+        if email_exist and usr_pass == password and usr_register:
             login_user(user)
-            return redirect("/dash")
+            return redirect('/dash')
         else:
             return abort(401)
+
+        db.commit()
+        db.close()
+        
     else:
         return render_template('admin/login.html')
+    
 
-@app.route("/register")
-def register():  
-
+@app.route("/register", methods=["GET", "POST"])
+def register(): 
+    db = psycopg2.connect(database = PSQL_DB_NAME, user = PSQL_USERNAME, password = PSQL_PASSWORD, host = PSQL_HOST, port = PSQL_PORT)
+    cur = db.cursor() 
+    
+    if request.method == 'POST':
+        id = '0'
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        emailadd = request.form['emailaddress']
+        password = request.form['password']
+       
+        cur.execute("select id from users;")
+        id_exist = bool(cur.rowcount)
+        if id_exist:
+            ids = cur.fetchall()
+            last_id = ids[-1][0]
+            id = str(last_id + 1)
+        # UPDATE users SET register='t' WHERE ID=0;
+          
+        cur.execute("select * from users where to_tsvector(email) @@ to_tsquery(%s);", (emailadd,))
+        email_exist = bool(cur.rowcount)
+        if email_exist:
+            flash(u'You Email have exist!','info')
+        else:
+            cur.execute("INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s)", (id, firstname, lastname, emailadd, password, 'f') )
+            flash(u'You Acconte Sucssesfuly Create','info')
+    db.commit()
+    db.close()
     return render_template('admin/register.html')     
 
 
 @app.route("/forgot-pass")
-def forgot_pass():  
+def forgot_pass():
 
     return render_template('admin/forgot-password.html')
 
@@ -87,18 +147,21 @@ def forgot_pass():
 @login_required
 def logout():
     logout_user()
-    return redirect("/dash")
+    return redirect('/dash')
 
 
 # handle login failed
 @app.errorhandler(401)
 def page_not_found(e):
-    return Response('<p>Login failed</p>')
+    
+    flash('your username or password is faild!', 'info')
+    return redirect('/login')
 
 # callback to reload the user object        
 @login_manager.user_loader
 def load_user(userid):
     return User(userid)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0' ,'5000' ,debug=True)
