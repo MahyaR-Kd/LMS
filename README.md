e3b800e124d3a6ba36e82d8a7eabf3e4e3993fdd
MahyaR-Kd <mahyarkakvand96@gmail.com>
2021-01-08 17:47:05 -0500

    add email sender and update db file for saving hash password and recode generator

--- README.md
+++ README.md
@@ -4,4 +4,12 @@
 
  ## TODO
  
- - [ ] save hash password in DB
+ - [x] create mail sender for register page
+ - [x] generate token for user
+ - [x] verify account
+ - [x] add limiter for expire verify code when 5 time use
+ - [x] make available take a new token
+ - [ ] add 404 handeling and page template
+ - [x] save hashed password in DB
+ - [ ] send email for welcome to site
+ - [ ] create mail sender for forgot password page
--- db.py
+++ db.py
@@ -11,15 +11,16 @@ with psycopg2.connect(database = PSQL_DB_NAME, user = PSQL_USERNAME, password = 
         # cur.execute("select * from information_schema.tables where table_name=%s", (table_name,))
         # tabel_exist = bool(cur.rowcount)
         # if tabel_exist:
-
+    # cur.execute("CREATE EXTENSION pgcrypto;")
     cur.execute("DROP TABLE IF EXISTS users;")
     cur.execute("""CREATE TABLE users(
-            ID int NOT NULL PRIMARY KEY,
-            FIRSTNAME CHAR(30), 
-            LASTNAME CHAR(30), 
-            EMAIL  CHAR(30), 
-            PASSWORD CHAR(30),
-            REGISTER BOOL);""")
+            ID SERIAL PRIMARY KEY,
+            FIRSTNAME CHAR(15), 
+            LASTNAME CHAR(15), 
+            EMAIL  TEXT NOT NULL, 
+            PASSWORD TEXT NOT NULL,
+            REGISTER BOOL,
+            TOKEN CHAR(6));""")
 
 
     print ("Table created successfully")
